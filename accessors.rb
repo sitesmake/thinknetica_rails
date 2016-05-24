37aefb6298543584340b6ff77fa86abb2d37f610
Sites Make <sitesmake@gmail.com>
2016-05-24 19:46:30 +0300

    Accessors module

---
+++ accessors.rb
@@ -0,0 +1,45 @@
+module Accessors
+  def attr_accessor_with_history(*names)
+    names.each do |name|
+      var_name = "@#{name}".to_sym
+      var_history_name = "@#{name}_history".to_sym
+
+      define_method(name) {
+        instance_variable_get(var_name)
+      }
+
+      define_method("#{name}=".to_sym) { |value|
+        history_value = (instance_variable_get(var_history_name) || []).push(value)
+        instance_variable_set(var_history_name, history_value)
+        instance_variable_set(var_name, value)
+      }
+
+      define_method("#{name}_history".to_sym) {
+        instance_variable_get(var_history_name)
+      }
+    end
+  end
+
+  def strong_attr_accessor(name, klass)
+    var_name = "@#{name}".to_sym
+
+    define_method(name) {
+      instance_variable_get(var_name)
+    }
+
+    define_method("#{name}=".to_sym) { |value|
+      if value.class.to_s == klass.to_s
+        instance_variable_set(var_name, value)
+      else
+        raise "Class of #{value} (#{value.class}) doesn't match #{klass}"
+      end
+    }
+  end
+end
+
+class Test
+  extend Accessors
+
+  attr_accessor_with_history :hello_meta
+  strong_attr_accessor :hello_strong, String
+end
